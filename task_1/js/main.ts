c6c93f53b8335a3a012de70de90852bbebc1fcc8
baby6969 <yafetzekarias23@fmail.com>
2025-09-14 18:18:23 -0400

    new file

---
+++ task_1/js/main.ts
@@ -0,0 +1,20 @@
+// Define the Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number;
+  [key: string]: any; // allow extra attributes
+}
+
+// Create a teacher object
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
