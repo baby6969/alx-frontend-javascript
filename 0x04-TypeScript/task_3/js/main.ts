de633d77c6f540e37bf7445aa61401dafb7cf37c
yafet zekariyas <93594782+baby6969@users.noreply.github.com>
2025-09-15 06:58:13 -0400

    Update main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,25 +1,21 @@
-/// <reference path=./crud.d.ts />
+/// <reference path="./crud.d.ts" />
 
-import { RowID, RowElement } from ./interface;
-import * as CRUD from ./crud.js;
+import { RowElement, RowID } from "./interface";
+import * as CRUD from "./crud.js";
 
 // Create a row object
 const row: RowElement = {
-  firstName: Guillaume,
-  lastName: Salva,
+  firstName: "Guillaume",
+  lastName: "Salva",
 };
 
 // Insert row
 const newRowID: RowID = CRUD.insertRow(row);
 
-// Update row with age
-const updatedRow: RowElement = {
-  ...row,
-  age: 23,
-};
+// Update row with age using spread syntax
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 
 // Delete row
 CRUD.deleteRow(newRowID);
-
